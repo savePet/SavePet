601b8879da22f7f844d379cb053306c21c3e4f9e
EnzoAkiraInoue <enzoakirainoue@gmail.com>
2023-06-21 20:08:55 -0300

    #17 feat/testes-fornecedor

--- savePetAPI/protegepet-api/src/main/java/br/com/uniamerica/protegepet/protegepetapi/service/FornecedorService.java
+++ savePetAPI/protegepet-api/src/main/java/br/com/uniamerica/protegepet/protegepetapi/service/FornecedorService.java
@@ -2,6 +2,8 @@ package br.com.uniamerica.protegepet.protegepetapi.service;
 
 import br.com.uniamerica.protegepet.protegepetapi.entity.Fornecedor;
 import br.com.uniamerica.protegepet.protegepetapi.entity.Servico;
+import br.com.uniamerica.protegepet.protegepetapi.entity.StatusCadastro;
+import br.com.uniamerica.protegepet.protegepetapi.entity.TipoFornecedor;
 import br.com.uniamerica.protegepet.protegepetapi.repository.FornecedorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,6 +28,7 @@ public class FornecedorService {
 
     @Transactional
     public Fornecedor save(Fornecedor fornecedor) {
+        validarCadastroFornecedor(fornecedor.getNome(), fornecedor.getSenha(), fornecedor.getEndereco(), fornecedor.getCnpj(), fornecedor.getTelefone(), fornecedor.getEmail(), fornecedor.getTipoFornecedor(), fornecedor.getServicos(), fornecedor.getStatusCadastro());
         return this.fornecedorRepository.save(fornecedor);
     }
 
@@ -35,6 +38,19 @@ public class FornecedorService {
             this.fornecedorRepository.save(fornecedor);
         } else {
             throw new RuntimeException("Id não encontrado");
+        }
+    }
+
+     public void validarCadastroFornecedor(String nome, String senha, String endereco, String cnpj, String telefone, String email, TipoFornecedor tipoFornecedor, List<Servico> servicos, StatusCadastro statusCadastro) {
+        if (nome == null && senha == null && endereco == null && cnpj == null && telefone == null && email == null && tipoFornecedor == null && statusCadastro == null) {
+            throw new RuntimeException("Preencha os campos por favor");
+        } else if (nome.length() > 40 || senha.length() > 15 || endereco.length() > 60 || cnpj.length() > 14 || telefone.length() > 9 || email.length() > 40) {
+            throw new RuntimeException("Limite de caracteres ultrapassado");
+        }
+        if (!(nome instanceof String) || !(senha instanceof String) || !(endereco instanceof String) || !(cnpj instanceof String) || !(telefone instanceof String) || !(email instanceof String) || !(tipoFornecedor instanceof Enum<?>) || !(statusCadastro instanceof Enum<?>)) {
+            throw new RuntimeException("Insira os valores corretamente");
+        } else {
+            System.out.println("Dados corretos");
         }
     }
 
