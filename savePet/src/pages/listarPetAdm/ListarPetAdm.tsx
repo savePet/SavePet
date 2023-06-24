492dd8d6d48780b1f4b1aebb16d56ead44909ef6
77Eduardo <103968776+77Eduardo@users.noreply.github.com>
2023-06-24 17:14:14 -0300

    feat #36  fazer modal de descricao pet

--- savePet/src/pages/listarPetAdm/ListarPetAdm.tsx
+++ savePet/src/pages/listarPetAdm/ListarPetAdm.tsx
@@ -2,7 +2,20 @@ import { Scroll } from '@phosphor-icons/react'
 import { MenuAdm } from '../../components/menuAdm/MenuAdm'
 import styles from '../listarPetAdm/index.module.css'
 
+import Modal from 'react-modal'
+import { useState } from 'react'
+import close1 from '../../assets/close1.svg'
+
 export const ListarPetAdm = () => {
+    const [modalIsOpen, setIsOpen] = useState(false)
+
+    const handleOpenModel = () => {
+        setIsOpen(true)
+    }
+
+    const handleCloseModel = () => {
+        setIsOpen(false)
+    }
     return (
         <section className={styles.section_container} id="ServiçoAdm">
             <div>
@@ -26,11 +39,51 @@ export const ListarPetAdm = () => {
                             <td>Cachorro</td>
                             <td>Grande</td>
                             <td>Masculino</td>
-                            <td><Scroll size={32} cursor="pointer"/></td>
+                            <td><Scroll size={32} onClick={handleOpenModel} cursor="pointer"/></td>
                         </tr>
                     </table>
                 </div> 
             </div>
+            <Modal isOpen={modalIsOpen}
+            className={styles.cardModal_container} 
+            overlayClassName={styles.modal__overlay}>
+                <div className={styles.closeModal}>
+                    <img src={close1} alt="iconi para fechar o modal" onClick={handleCloseModel} />
+                </div>
+                <div className={styles.modal_container}>
+                    <div className={styles.descricaoModal}>
+                        <p className={styles.tituloModal}>Animal</p>
+                        <div className={styles.descricao}>
+                            <p className={styles.infoModal}>ID:</p>
+                            <p className={styles.descricao_info}>Not entered</p>
+                        </div>
+                        <div className={styles.descricao}>
+                            <p className={styles.infoModal}>Raça:</p> 
+                            <p className={styles.descricao_info}>Not entered</p>
+                        </div>
+                        <div className={styles.descricao}>
+                            <p className={styles.infoModal}>StatusSaude:</p>
+                            <p className={styles.descricao_info}>Not entered</p>
+                        </div>
+                        <div className={styles.descricao}>
+                            <p className={styles.infoModal}>Especie:</p>
+                            <p className={styles.descricao_info}>Not entered</p>
+                        </div>
+                        <div className={styles.descricao}>
+                            <p className={styles.infoModal}>PorteFisico:</p>
+                            <p className={styles.descricao_info}>Not entered</p>
+                        </div>
+                        <div className={styles.descricao}>
+                            <p className={styles.infoModal}>Genero:</p>
+                            <p className={styles.descricao_info}>Not entered</p>
+                        </div>
+                        <div className={styles.descricao}>
+                            <p className={styles.infoModal}>Cor:</p>
+                            <p className={styles.descricao_info}>Not entered</p>
+                        </div>
+                    </div>
+                </div>
+            </Modal>
         </section>
     )
 }
