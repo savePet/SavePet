be67c51541a3f6220274fc05b84eefc18123001e
77Eduardo <103968776+77Eduardo@users.noreply.github.com>
2023-06-25 15:36:54 -0300

    feat #39 modal de descrição do pet

--- savePet/src/pages/listarProtetor/ListarProtetorAdm.tsx
+++ savePet/src/pages/listarProtetor/ListarProtetorAdm.tsx
@@ -2,7 +2,20 @@ import { Scroll } from "@phosphor-icons/react"
 import styles from "../listarProtetor/index.module.css"
 import { MenuAdm } from "../../components/menuAdm/MenuAdm"
 
+import Modal from 'react-modal'
+import { useState } from 'react'
+import close1 from '../../assets/close1.svg'
+
 export const ListarProtetorAdm = () => {
+    const [modalIsOpen, setIsOpen] = useState(false)
+
+    const handleOpenModel = () => {
+        setIsOpen(true)
+    }
+
+    const handleCloseModel = () => {
+        setIsOpen(false)
+    }
     return (
         <section className={styles.section_container}>
             <div>
@@ -24,11 +37,51 @@ export const ListarProtetorAdm = () => {
                             <td className={styles.statusProtetor}>Aceito</td>
                             <td>segunda a sexta</td>
                             <td>example@gmail.com</td>
-                            <td><Scroll size={32}cursor="pointer"/></td>
+                            <td><Scroll size={32} onClick={handleOpenModel} cursor="pointer"/></td>
                         </tr>
                     </table>
                 </div> 
             </div>
+            <Modal isOpen={modalIsOpen}
+            className={styles.cardModal_container} 
+            overlayClassName={styles.modal__overlay}>
+                <div className={styles.closeModal}>
+                    <img src={close1} alt="iconi para fechar o modal" onClick={handleCloseModel} />
+                </div>
+                <div className={styles.modal_container}>
+                    <div className={styles.descricaoModal}>
+                        <p className={styles.tituloModal}>Protetor</p>
+                        <div className={styles.descricao}>
+                            <p className={styles.infoModal}>ID:</p>
+                            <p className={styles.descricao_info}>Not entered</p>
+                        </div>
+                        <div className={styles.descricao}>
+                            <p className={styles.infoModal}>Nome:</p> 
+                            <p className={styles.descricao_info}>Not entered</p>
+                        </div>
+                        <div className={styles.descricao}>
+                            <p className={styles.infoModal}>Endereço:</p>
+                            <p className={styles.descricao_info}>Not entered</p>
+                        </div>
+                        <div className={styles.descricao}>
+                            <p className={styles.infoModal}>Telefone:</p>
+                            <p className={styles.descricao_info}>Not entered</p>
+                        </div>
+                        <div className={styles.descricao}>
+                            <p className={styles.infoModal}>Email:</p>
+                            <p className={styles.descricao_info}>Not entered</p>
+                        </div>
+                        <div className={styles.descricao}>
+                            <p className={styles.infoModal}>Disponibilidade de Acolhimento:</p>
+                            <p className={styles.descricao_info}>Not entered</p>
+                        </div>
+                        <div className={styles.descricao}>
+                            <p className={styles.infoModal}>Status Cadastro:</p>
+                            <p className={styles.descricao_info}>Not entered</p>
+                        </div>
+                    </div>
+                </div>
+            </Modal>
         </section>
     )
 }
