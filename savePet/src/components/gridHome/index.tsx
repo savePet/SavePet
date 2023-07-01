256be956bbac6f0b9a7b8cf67f06e82ebf95db02
Anderson Daniel <anderbreiksondaniel@gmail.com>
2023-07-01 19:13:15 -0300

    teste

--- savePet/src/components/gridHome/index.tsx
+++ savePet/src/components/gridHome/index.tsx
@@ -34,8 +34,7 @@ export const GridHome = () => {
                             link="Evento"
                         />
                     </div>
-                    <div>
-
+                    <div className={styles.doguinho}>
                     </div>
                 </div>
                 <div className={styles.grid}  >
@@ -64,6 +63,9 @@ export const GridHome = () => {
                         title='Clinicas Parceiras'
                         link=""
                     />
+                    <div className={styles.doguinho}>
+
+                    </div>
                 </div>
             </div>
         </section>
