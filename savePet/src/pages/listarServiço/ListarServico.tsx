9017caa612f14459cef875d756386e02061bf7a7
77Eduardo <103968776+77Eduardo@users.noreply.github.com>
2023-07-02 01:14:39 -0300

    feat #50 integracao com API ListarServicoAdm

--- savePet/src/pages/listarServiço/ListarServico.tsx
+++ savePet/src/pages/listarServiço/ListarServico.tsx
@@ -2,13 +2,26 @@ import { Scroll } from '@phosphor-icons/react'
 import { MenuAdm } from '../../components/menuAdm/MenuAdm'
 import styles from './index.module.css'
 import Modal from 'react-modal'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import close1 from '../../assets/close1.svg'
+import { api } from '../../configs'
+import { Fornecedores, Servico } from '../../model/Servico'
 
 export const ListarServico = () => {
-    const [modalIsOpen, setIsOpen] = useState(false)
+    const [modalIsOpen, setIsOpen] = useState(false);
+    const [getServicolist, setServicolist] = useState([]);
+    const [getSelectServico, setSelectServico] = useState<Servico | null>(null);
+    const [getSelectFornecedor, setSelectFornecedor] = useState<Fornecedores | null>(null);
 
-    const handleOpenModel = () => {
+    useEffect(() => { 
+        api.get('/api/fornecedores')
+        .then(res => setServicolist(res.data))
+        .catch(err => console.log(err));
+     }, []);
+
+    const handleOpenModel = (fornecedor: Fornecedores, servico: Servico) => {
+        setSelectFornecedor(fornecedor);
+        setSelectServico(servico);
         setIsOpen(true)
     }
 
@@ -30,50 +43,61 @@ export const ListarServico = () => {
                             <th>QUANTIDADE</th>
                             <th>CONTATO FORNECEDOR</th>
                         </tr>
-                        <tr>
-                            <td >1</td>
-                            <td className={styles.nome}>Raçao</td>
-                            <td className={styles.tipocadastro}>0</td>
-                            <td className={styles.status}>20</td>
-                            <td>45 9999-9999/exemplo@gmail.com</td>
-                            <td><Scroll size={32} onClick={handleOpenModel} cursor="pointer"/></td>
-                        </tr>
+                        {getServicolist.map((item: Fornecedores) => (
+                            <>
+                                {item.servicos.map(servico => (
+                                    <tr>
+                                        <td>{servico.id}</td>
+                                        <td className={styles.nome}>{servico.nome}</td>
+                                        <td className={styles.tipocadastro}>{servico.preco}</td>
+                                        <td className={styles.status}>{servico.quantidade}</td>
+                                        <td>{item.telefone} / {item.email}</td>
+                                        <td><Scroll size={32} onClick={() => handleOpenModel(item, servico)} 
+                                        cursor="pointer" data-testid="icon-scroll"/></td>
+                                    </tr>
+                                ))}
+                            </>
+                        ))}
                     </table>
                 </div> 
             </div>
             <Modal isOpen={modalIsOpen}
             className={styles.cardModal_container} 
-            overlayClassName={styles.modal__overlay}>
+            overlayClassName={styles.modal__overlay} data-testid="modal-content">
                 <div className={styles.closeModal}>
                     <img src={close1} alt="iconi para fechar o modal" onClick={handleCloseModel} />
                 </div>
                 <div className={styles.modal_container}>
                     <div className={styles.descricaoModal}>
                         <p className={styles.tituloModal}>Serviço do Fornecedor</p>
-                        <div className={styles.descricao}>
-                            <p className={styles.infoModal}>ID:</p>
-                            <p className={styles.descricao_info}>Not entered</p>
-                        </div>
-                        <div className={styles.descricao}>
-                            <p className={styles.infoModal}>Nome:</p> 
-                            <p className={styles.descricao_info}>Not entered</p>
-                        </div>
-                        <div className={styles.descricao}>
-                            <p className={styles.infoModal}>Preço:</p>
-                            <p className={styles.descricao_info}>Not entered</p>
-                        </div>
-                        <div className={styles.descricao}>
-                            <p className={styles.infoModal}>Quantidade:</p>
-                            <p className={styles.descricao_info}>Not entered</p>
-                        </div>
-                        <div className={styles.descricao}>
-                            <p className={styles.infoModal}>Email:</p>
-                            <p className={styles.descricao_info}>Not entered</p>
-                        </div>
-                        <div className={styles.descricao}>
-                            <p className={styles.infoModal}>Telefone:</p>
-                            <p className={styles.descricao_info}>Not entered</p>
-                        </div>
+                        {getSelectServico && (
+                            <>
+                            <div className={styles.descricao}>
+                                <p className={styles.infoModal}>ID:</p>
+                                <p className={styles.descricao_info}>{getSelectServico.id}</p>
+                            </div>
+                            <div className={styles.descricao}>
+                                <p className={styles.infoModal}>Nome:</p> 
+                                <p className={styles.descricao_info}>{getSelectServico.nome}</p>
+                            </div>
+                            <div className={styles.descricao}>
+                                <p className={styles.infoModal}>Preço:</p>
+                                <p className={styles.descricao_info}>{getSelectServico.preco}</p>
+                            </div>
+                            <div className={styles.descricao}>
+                                <p className={styles.infoModal}>Quantidade:</p>
+                                <p className={styles.descricao_info}>{getSelectServico.quantidade}</p>
+                            </div>
+                            <div className={styles.descricao}>
+                                <p className={styles.infoModal}>Email:</p>
+                                <p className={styles.descricao_info}>{getSelectFornecedor?.email}</p>
+                            </div>
+                            <div className={styles.descricao}>
+                                <p className={styles.infoModal}>Telefone:</p>
+                                <p className={styles.descricao_info}>{getSelectFornecedor?.telefone}</p>
+                            </div>
+                            </>
+                        )}
                     </div>
                 </div>
             </Modal>
